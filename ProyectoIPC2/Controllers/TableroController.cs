c5235ecb9d89eadfbbea7c496c0940291ee6e0db
Macario12 <arielmacario.11@gmail.com>
2020-08-30 20:56:18 -0600

    Primeras Vistas

---
+++ ProyectoIPC2/Controllers/TableroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoIPC2.Controllers
+{
+    public class TableroController : Controller
+    {
+        // GET: Tablero
+        public ActionResult Tablero()
+        {
+            return View();
+        }
+    }
+}
